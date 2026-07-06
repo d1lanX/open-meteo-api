eac85b585fbf44af86f7a9eb3fc2e9ef8cd8edfd
d1lanX <89811097+d1lanX@users.noreply.github.com>
2026-07-06 15:28:12 -0500

    deletes and updates compose file

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-# One-off syncs (static terrain data, initial forecast data)
-open-meteo sync copernicus_dem90 static
-open-meteo sync ecmwf_ifs025 temperature_2m
-
-# Start the API server
-exec open-meteo serve
